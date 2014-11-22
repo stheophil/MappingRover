0aff2415ddce413779e286e7723ef0baec9fddfb
Sebastian Theophil <sebastian@theophil.net>
2014-11-22 14:47:48 +0100

    Initial Commit

---
+++ robotcontrol2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  robotcontrol2
+//
+//  Created by Sebastian Theophil on 22.11.14.
+//  Copyright (c) 2014 Sebastian Theophil. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ robotcontrol2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  robotcontrol2
+//
+//  Created by Sebastian Theophil on 22.11.14.
+//  Copyright (c) 2014 Sebastian Theophil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
