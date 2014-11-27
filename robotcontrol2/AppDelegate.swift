dec5a0a396bfa2455be4d11aed694210b72d614a
Sebastian Theophil <sebastian@theophil.net>
2014-11-27 21:39:36 +0100

    Enumerate BLE services

--- robotcontrol2/AppDelegate.swift
+++ robotcontrol2/AppDelegate.swift
@@ -10,9 +10,6 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
     }
--- robotcontrol2/ViewController.swift
+++ robotcontrol2/ViewController.swift
@@ -7,9 +7,84 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
 class DataModel : NSObject {
     override init() {}
+}
+
+class BLE : NSObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    override init() {
+        super.init()
+        centralmgr = CBCentralManager(delegate:self, queue:nil)
+    }
+    
+    func scanForPeripherals() {
+        println("start scanning")
+        
+        centralmgr.scanForPeripheralsWithServices(UUID_SERVICES, options:nil)
+    }
+    
+    // CBCentralManagerDelegate interface
+    func centralManagerDidUpdateState(central: CBCentralManager!) {
+        // check central.state
+        println("centralManagerDidUpdateState: \(central.state.rawValue)")
+        
+        if(central.state==CBCentralManagerState.PoweredOn) {
+            scanForPeripherals()
+        }
+        // TODO: Disconnect
+    }
+    
+    func centralManager(central: CBCentralManager!,
+        didDiscoverPeripheral peripheral: CBPeripheral!,
+        advertisementData: [NSObject : AnyObject]!,
+        RSSI: NSNumber!)
+    {
+        println("Discovered peripheral \(peripheral.name)")
+        
+        peripheralActive = peripheral
+        peripheralActive?.delegate = self
+        centralmgr.connectPeripheral(peripheral, options: nil)
+        
+        centralmgr.stopScan()
+    }
+    
+    func centralManager(central : CBCentralManager!,
+        didConnectPeripheral peripheral : CBPeripheral!)
+    {
+        println("Connected peripheral \(peripheral.name)")        
+        peripheralActive?.discoverServices(nil) // No need to filter services.
+    }
+    
+    // CBPeripheralDelegate interface
+    func peripheral(peripheral: CBPeripheral!,
+        didDiscoverServices error: NSError!)
+    {
+        assert(error==nil)
+        // assert(peripheralActive?.services.count==1)
+        println("Discovered service")
+        
+        peripheralActive?.discoverCharacteristics(nil, forService: peripheralActive?.services[0] as CBService)
+    }
+    
+    func peripheral(peripheral: CBPeripheral!,
+        didDiscoverCharacteristicsForService service: CBService!,
+        error: NSError!)
+    {
+        assert(error==nil)
+        // assert(service.characteristics.count==2)
+        println("Discovered characteristics")
+        
+        for characteristic in service.characteristics {
+            
+        }
+    }
+    
+    var centralmgr : CBCentralManager!
+    var peripheralActive : CBPeripheral?
+    
+    let UUID_SERVICES = [CBUUID(string:"713D0000-503E-4C75-BA94-3148F18D941E")]
 }
 
 class ViewController: NSViewController {
@@ -25,6 +100,8 @@ class ViewController: NSViewController {
         
         model = DataModel()
         viewRender.model = model
+        
+        ble = BLE()
     }
 
     override var representedObject: AnyObject? {
@@ -34,9 +111,10 @@ class ViewController: NSViewController {
     }
     
     var model : DataModel!;
+    var ble : BLE!;
     
-    @IBOutlet weak var viewRender: QuartzView!
-    @IBOutlet weak var textview: NSTextView!
-    @IBOutlet weak var btnMeasurement: NSButton!
+    @IBOutlet var viewRender: QuartzView!
+    @IBOutlet var textview: NSTextView!
+    @IBOutlet var btnMeasurement: NSButton!
 }
 
